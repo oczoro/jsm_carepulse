239db0147e6f514aff39320c3b321970fed54875
oczoro <orozcoykevin@gmail.com>
2024-10-02 02:49:07 -0400

    first commit

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import { AppointmentForm } from '@/components/forms/AppointmentForm';
+import { getPatient } from '@/lib/actions/patient.actions';
+
+export default async function NewAppointment({ params: { userId } }: SearchParamProps) {
+  const patient = await getPatient(userId);
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <div className="w-fit">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-full"
+            />
+          </div>
+
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePulse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://764848908a33e63f4c15b023beeae5c2@o4508051261816832.ingest.us.sentry.io/4508051264176128",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
